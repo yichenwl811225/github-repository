3247ded05d1db1a9314d61f6e89e3859bd12cfc6
wangliang <yichenwl@163.com>
2014-09-24 16:41:36 +0800

    wangliang

--- myGit/ViewController.swift
+++ myGit/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 //        111111
+//        222222
 //        dsfdsf
         // Do any additional setup after loading the view, typically from a nib.
     }
