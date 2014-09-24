309aa8e44ebdda90396c04e0ba6033a3bd056cfd
jinhua <981479486@qq.com>
2014-09-24 15:48:50 +0800

    Initial Commit

---
+++ myGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myGit
+//
+//  Created by 金华 on 14-9-24.
+//  Copyright (c) 2014年 mysteel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
